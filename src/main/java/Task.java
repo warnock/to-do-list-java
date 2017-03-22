c1fc03314369eb25a4c56e83a11dd6ef2234a344
Chris Carr and Megan Warnock <chris@>
2017-03-22 10:32:42 -0700

    add isCompleted and getCreatedAt and pass tests

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,25 @@
+import java.time.LocalDateTime;
+
 public class Task {
   private String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
 
   public Task(String description) {
     mDescription = description;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
   }
 
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
@@ -14,4 +15,17 @@ public class TaskTest {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
   }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+
 }
