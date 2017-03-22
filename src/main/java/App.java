a57f2a11d3921892161a484a88c4858c5647127b
Chris Carr and Megan Warnock <chris@>
2017-03-22 16:47:10 -0700

    refactore links

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -41,8 +41,13 @@ public class App {
 
     get("tasks/:id", (request, response)-> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+
       Task task = Task.find(Integer.parseInt(request.params(":id")));
       model.put("task", task);
+
       model.put("template", "templates/task.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
